19c96e706fc8655183e4dcc6ba402b57b54c7960
SoA-Lee <llsa0927@naver.com>
2021-05-10 21:34:27 +0900

    toyproject

---
+++ ThreeButton/app/src/main/java/com/example/threebutton/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.threebutton;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.MenuInflater;
+import android.view.MotionEvent;
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    LinearLayout baseLayout;
+    Button button1;
+    Button addBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("배경색 바꾸기");
+
+
+
+            }
+
+
+
+
+}
